55dca083fdd83228c7545bfa8c615ed4f30588e1
Jane <jenya.kissel@gmail.com>
2026-04-17 14:34:14 +0200

    Store the JSON data in a DataFrame

--- Twitter_Mining.py
+++ Twitter_Mining.py
@@ -1,16 +1,9 @@
 # Importing Dependencies
-import os
-from dotenv import load_dotenv
-import tweepy
 
-# Loading Environment Variables
-load_dotenv() # loads .env file
+# Data Manipulation
+import pandas as pd
+import numpy as np
 
-bearer_token = os.getenv('TWITTER_BEARER_TOKEN')
+tweets_df = pd.read_json("data/raw/nikelululemonadidas_tweets.jsonl", lines=True)
 
-# Initializing Twitter API Client
-client = tweepy.Client(bearer_token=bearer_token)
-
-query = "music"
-
-# Fetching Tweets
+# You can filter the dataframe to subsets of lululemon, nike, and adidas tweets using the 'brand' column after cleaningthe data
