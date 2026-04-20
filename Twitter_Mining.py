3f7c005b7228b1f39c2951e6f543a9609bc77f3a
Jane <jenya.kissel@gmail.com>
2026-04-20 09:44:59 +0200

    Display first 5 tweet objects

--- Twitter_Mining.py
+++ Twitter_Mining.py
@@ -7,3 +7,8 @@ import numpy as np
 tweets_df = pd.read_json("data/raw/nikelululemonadidas_tweets.jsonl", lines=True)
 
 # You can filter the dataframe to subsets of lululemon, nike, and adidas tweets using the 'brand' column after cleaningthe data
+
+# Display basic info and properties of the dataframe
+print(f"Basic Info: {tweets_df.info()}")
+
+print(f"First 5 rows of the dataframe: {tweets_df.head()}")
