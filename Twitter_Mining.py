73cb60396d914427734c4da099411cbd9e392517
Jane <jenya.kissel@gmail.com>
2026-04-20 13:51:44 +0200

    Removing duplicates and empty rows, importing text preprocessing libraries

--- Twitter_Mining.py
+++ Twitter_Mining.py
@@ -3,10 +3,21 @@
 # Data Manipulation
 import pandas as pd
 import numpy as np
+from collections import Counter
+
+# Text Preprocessing
+import re
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 tweets_df = pd.read_json("data/raw/nikelululemonadidas_tweets.jsonl", lines=True)
-
-# You can filter the dataframe to subsets of lululemon, nike, and adidas tweets using the 'brand' column after cleaningthe data
 
 # Display basic info and properties of the dataframe
 tweets_df.info()
@@ -15,7 +26,17 @@ print(f"\nFirst 5 rows of the 'full_text' column: {tweets_df['full_text'].head()
 print(f"\nFirst row of the dataframe: {tweets_df.iloc[0]}")
 
 # Extract attributes like id, created_at, retweet_count, text from the tweets and create a new dataframe with these attributes
-tweets_subset_df = tweets_df[['id_str', 'created_at', 'retweet_count', 'full_text']]
+tweets_df = tweets_df[['id_str', 'created_at', 'retweet_count', 'full_text']].copy()
+
+# Remove duplicates & Empty Rows
+tweets_df.drop_duplicates(inplace=True)
+tweets_df = tweets_df.dropna(inplace=True)
+
+
+# Entity Analysis
+
+
+
 
 # Define a function to detect and extract brand mentions in the tweet text
 def get_brand(tweet):
